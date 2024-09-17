c39d6036934662378553840405545a064a384d33
polsonBFA <peter.olson@bfacademy.de>
2024-09-17 04:32:26 +0000

    Line added

--- HelloCodespace.java
+++ HelloCodespace.java
@@ -2,5 +2,8 @@ public class HelloCodespace{
    public static void main (String[] args)
    {
       System.out.println("Hello Java Codespace");  //turn off the inlay hints
+      int b = 7;
+      System.out.println(Math.pow(b, 3));
+      //
    }
 }
