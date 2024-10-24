929ed9da4f4ba43a3df8a05796119a924af51008
polsonBFA <peter.olson@bfacademy.de>
2024-10-24 13:09:29 +0000

    Added 3.7 Example

---
+++ Unit 3/Example3_7.java
@@ -0,0 +1,15 @@
+public class Example3_7 {
+   public static void main(String[] args)
+   {
+      String sky = new String("blue"); 
+      String sky2 = new String("blue");
+      String today = "Thursday";
+    
+      System.out.println(sky.equals("blue") || !today.equals("Monday"));
+      //System.out.println(sky == sky2);
+
+
+   
+
+   }
+}
