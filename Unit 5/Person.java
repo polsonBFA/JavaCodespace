9dbbca585a507b8e7d97a181530602c3e6b18f6d
polsonBFA <peter.olson@bfacademy.de>
2024-11-27 13:10:32 +0000

    Updates

---
+++ Unit 5/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+   
+   //define instance variables
+   private String name;
+   private String email;
+   private String phone;
+  
+   //Constructor - how to build a new person
+   public Person (String initName, String initEmail, String initPhone)
+   {
+      name = initName;  //initializing instance variables...
+      email = initEmail;
+      phone = initPhone;
+   }
+
+   public void print()
+   {
+      System.out.println("Name: " + name);
+      System.out.println("Email: " + email);
+      System.out.println("Phone: " + phone);
+   }
+   public static void main(String[] args)
+   {
+      Person person1 = new Person("Peter", "peter.olson@bfacademy.de", "112");
+      person1.print();
+   }
+
+}
